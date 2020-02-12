00b1aad9b1d4115608e7636d704c426534c0cc1a
uriel zacarias <urielzacarias@gmail.com>
2020-02-12 12:17:40 -0800

    added spread(...) opperator to create a new array to avoid editing the original state array

--- App.js
+++ App.js
@@ -25,7 +25,8 @@ class App extends Component {
   }
 
   deleteUserHandler = (userIndex) => {
-    const users = this.state.users;
+    // const users = this.state.users.slice();
+    const users = [...this.state.users];
     users.splice(userIndex, 1);
     this.setState({users:users});
   }

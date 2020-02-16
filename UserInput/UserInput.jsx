c82111f3d4ed100304f65c875b8058e601f308e2
uriel zacarias <urielzacarias@gmail.com>
2020-02-15 23:59:20 -0800

    Created inputfield with event listenr that outputs text length

--- UserInput/UserInput.jsx
+++ UserInput/UserInput.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
-const userInput = (props) => {
+const char = (props) => {
     const inputStyle = {
-        border: '2px solid blue'
+        display: 'inline-block',
+        padding: '16px',
+        margin: ' 16px',
+        border: '1px solid black', 
+        textAlign: 'center'
     };
     
-    return [
-        <div className="UserInput">
+    return (        
+        <div style={style} onClick={props.clicked}>
+            {props.character}
         </div>
-    ]
+    );
+    
 };
 
-export default userInput;
+export default char;

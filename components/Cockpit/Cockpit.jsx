37e7b3ae491cc4b53c76d7bd1d4e533ce0c5a669
uriel zacarias <urielzacarias@gmail.com>
2020-02-21 22:19:44 -0800

    added useEffect react hooks function

--- components/Cockpit/Cockpit.jsx
+++ components/Cockpit/Cockpit.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React , {useEffect, useState} from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+        // Http request...
+        setTimeout(() => {
+            alert('Save data to cloud!');
+        }, 1000);
+    }, []);
+
+
     //let classes = ['red', 'bold'].join(' ');
     const assignedClasses = [];
     let btnClass = '';

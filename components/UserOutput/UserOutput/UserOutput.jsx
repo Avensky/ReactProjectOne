99a5b8c78992d3cdfea1727ee8854e9da95aa25f
uriel zacarias <urielzacarias@gmail.com>
2020-02-24 23:55:24 -0800

    created aux component"

--- components/UserOutput/UserOutput/UserOutput.jsx
+++ components/UserOutput/UserOutput/UserOutput.jsx
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import classes from './UserOutput.css';
-
+import Auxilary from '../../../hoc/Auxilary';
+import Aux from '../../../hoc/Auxilary';
 class User extends Component {
     render (){
         console.log('[UserOuput.jsx] rendering...');
-        return (
-            <div className={classes.UserOutput}>
-                <p onClick={this.props.click}>Username: {this.props.username}</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.username} />
-            </div>        
-        );
+        return 
+        <Aux>
+            <p key="i1" onClick={this.props.click}>
+                Username: {this.props.username}
+            </p>,
+            <p key="i2">{this.props.children}</p>,
+            <input 
+                key="i3"
+                type="text" 
+                onChange={this.props.changed} 
+                value={this.props.username} 
+            />        
+        </Aux>;
 
     }
 }

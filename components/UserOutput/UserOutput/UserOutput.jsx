c61c5aa2d5de1839800570001f7358d25bf2eaea
uriel zacarias <urielzacarias@gmail.com>
2020-02-27 23:22:44 -0800

    created propTypes in order to show an error when an unexpected data type is entered

--- components/UserOutput/UserOutput/UserOutput.jsx
+++ components/UserOutput/UserOutput/UserOutput.jsx
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import classes from './UserOutput.css';
 import Auxilary from '../../../hoc/Auxilary';
 import withClass from '../../../hoc/withClass';
+
 class User extends Component {
     render (){
         console.log('[UserOuput.jsx] rendering...');
         return (
         <Auxilary>
             <p key="i1" onClick={this.props.click}>
-                Username: {this.props.username}
-            </p>,
-            <p key="i2">{this.props.children}</p>,
+                Username: {this.props.username}</p>
+            <p key="i2">{this.props.children}</p>
             <input 
                 key="i3"
                 type="text" 
@@ -21,5 +22,12 @@ class User extends Component {
         ;
     }
 }
+
+User.propTypes = {
+    click: PropTypes.func,
+    username: PropTypes.string,
+    changed: PropTypes.func,
+
+};
 
 export default withClass(User, classes.UserOutput);

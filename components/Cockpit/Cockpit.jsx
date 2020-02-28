2d9de65327ac6728b7a3fa922d74305efe8facf5
uriel zacarias <urielzacarias@gmail.com>
2020-02-27 23:52:58 -0800

    added useRef hook to automatically click the goggle button

--- components/Cockpit/Cockpit.jsx
+++ components/Cockpit/Cockpit.jsx
@@ -1,13 +1,16 @@
-import React , {useEffect} from 'react';
+import React , {useEffect, useRef} from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+    const toggleBtnRef = useRef(null);
+
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // Http request...
-        setTimeout(() => {
-            alert('Save data to cloud!');
-        }, 1000);
+        //setTimeout(() => {
+        //   alert('Save data to cloud!');
+        //}, 1000);
+        toggleBtnRef.current.click();
         return () => {
             console.log('[Cockpit.js] cleanup work in useEffect')
         };
@@ -40,6 +43,7 @@ const cockpit = (props) => {
             <h1>{props.title}</h1>
             <p className={assignedClasses.join( ' ' )}>This is really working!</p>
             <button 
+                ref={toggleBtnRef}
                 className={btnClass}
                 onClick={props.clicked}>
                 Show Users
